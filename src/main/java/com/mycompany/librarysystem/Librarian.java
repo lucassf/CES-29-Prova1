567846591f56b960c336bce60c9f89e4b09b218c
lucassf <lucassoares-2010@hotmail.com>
2017-04-23 02:10:45 -0300

    V 1.0

--- src/main/java/com/mycompany/librarysystem/Librarian.java
+++ src/main/java/com/mycompany/librarysystem/Librarian.java
@@ -1,64 +1,66 @@
 package com.mycompany.librarysystem;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 
 public class Librarian {
     
-   private final UserDatabase userDatabase;
-   private final BookDatabase database;
-   private final ArrayList<Book> availableBooks;
-   private final ArrayList<Book> borrowedBooks;
-   
-   public Librarian(UserDatabase database,BookDatabase bookdatabase) {
-       this.userDatabase = database;
-       this.database = bookdatabase;
-       if (bookdatabase!=null)availableBooks = bookdatabase.getBookList();
-       else availableBooks = new ArrayList<>();
-       borrowedBooks = new ArrayList<>();
-   }
-   
-    public boolean addUser(User user){
+    private final UserDatabase userDatabase;
+    private final BookDatabase database;
+    private final ArrayList<Book> availableBooks;
+    private final ArrayList<Book> borrowedBooks;
+    
+    public Librarian(UserDatabase database, BookDatabase bookdatabase) {
+        this.userDatabase = database;
+        this.database = bookdatabase;
+        if (bookdatabase != null) {
+            availableBooks = bookdatabase.getBookList();
+        } else {
+            availableBooks = new ArrayList<>();
+        }
+        borrowedBooks = new ArrayList<>();
+    }
+    
+    public boolean addUser(User user) {
         return userDatabase.addUser(user);
     }
-
-    public boolean deleteUser(User user){
+    
+    public boolean deleteUser(User user) {
         return userDatabase.deleteUser(user.getId());
     }
     
-    public boolean addBook(Book book){
+    public boolean addBook(Book book) {
         availableBooks.add(book);
         return database.addBook(book);
     }
-        
-    public boolean deleteBook(Book book){
-        if (availableBooks.contains(book)){
+    
+    public boolean deleteBook(Book book) {
+        if (availableBooks.contains(book)) {
             availableBooks.remove(book);
         }
         return database.deleteBook(book.getListId());
     }
     
-    public boolean blockUserDefinedTime(User user,int time){
+    public boolean blockUserDefinedTime(User user, int time) {
         user.timeBlockUser(time);
         return userDatabase.changeUserInfo(user);
     }
     
-    public boolean financialBlockUser(User user){
+    public boolean financialBlockUser(User user) {
         user.financialBlockUser();
         sendInfoToFinancialSector();
         return userDatabase.changeUserInfo(user);
     }
     
-    public boolean unblockUser(User user){
+    public boolean unblockUser(User user) {
         user.unblockUser();
         return userDatabase.changeUserInfo(user);
     }
     
-    public boolean lendBook(User user,Book book){
-        boolean possible = user.getStatus().equals(UserStatus.UNBLOCKED)&&
-                book.getStatus().equals(BookStatus.AVAILABLE);
-        if (possible){
+    public boolean lendBook(User user, Book book) {
+        boolean possible = user.getStatus().equals(UserStatus.UNBLOCKED)
+                && book.getStatus().equals(BookStatus.AVAILABLE);
+        if (possible) {
             setReturnDate(book);
             book.setStatus(BookStatus.BORROWED);
             borrowedBooks.add(book);
@@ -69,12 +71,12 @@ public class Librarian {
         return possible;
     }
     
-    public boolean returnBook(User user,Book book){
+    public boolean returnBook(User user, Book book) {
         boolean possible = user.hasBorrowedBook(book);
-        if (possible){
+        if (possible) {
             book.setStatus(BookStatus.AVAILABLE);
             int time = verifyReturnDate(book);
-            if (time>0){
+            if (time > 0) {
                 blockUserDefinedTime(user, time);
             }
             availableBooks.add(book);
@@ -85,9 +87,9 @@ public class Librarian {
         return possible;
     }
     
-    public boolean lostBook(User user,Book book){
+    public boolean lostBook(User user, Book book) {
         boolean possible = user.hasBorrowedBook(book);
-        if (possible){
+        if (possible) {
             financialBlockUser(user);
             book.setStatus(BookStatus.LOST);
             userDatabase.changeUserInfo(user);
@@ -97,40 +99,42 @@ public class Librarian {
         return possible;
     }
     
-    public String showAvailableBookList(){
-        String ret="";
+    public String showAvailableBookList() {
+        String ret = "";
         availableBooks.sort(new BookComparator());
-        for(Book book:availableBooks){
-            ret+=book.toString()+"\n";
+        for (Book book : availableBooks) {
+            ret += book.toString() + "\n";
         }
         
         return ret;
     }
     
-    public String showBorrowedBookList(){
-        String ret="";
+    public String showBorrowedBookList() {
+        String ret = "";
         borrowedBooks.sort(new BookComparator());
-        for(Book book:borrowedBooks){
-            ret+=book.toString()+"\n";
+        for (Book book : borrowedBooks) {
+            ret += book.toString() + "\n";
         }
         return ret;
     }
     
-    private int verifyReturnDate(Book book){
+    private int verifyReturnDate(Book book) {
         Calendar calendar = Calendar.getInstance();
-        int time = calendar.get(Calendar.DAY_OF_YEAR)-
-                book.getReturnDate().get(Calendar.DAY_OF_YEAR);
-        if (time<=0)return 0;
+        int time = calendar.get(Calendar.DAY_OF_YEAR)
+                - book.getReturnDate().get(Calendar.DAY_OF_YEAR);
+        if (time <= 0) {
+            return 0;
+        }
         return time;
     }
     
-    private void setReturnDate(Book book){
+    private void setReturnDate(Book book) {
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR,7);//7 days to return the book
+        calendar.add(Calendar.DAY_OF_YEAR, 7);//7 days to return the book
         book.setReturnDate(calendar);
     }
     
-    private void sendInfoToFinancialSector(){
+    private void sendInfoToFinancialSector() {
         //Sends the info of a pending payment to the financial Sector
     }
 }
